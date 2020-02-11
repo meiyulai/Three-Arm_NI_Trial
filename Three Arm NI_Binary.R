7f299323804b8eeef87d74f6e214971c609f34dc
Mei-Yu Lai <shellielai@gmail.com>
2020-02-11 16:33:35 -0500

    add R codes

---
+++ Three Arm NI_Binary.R
@@ -0,0 +1,57 @@
+library(mvtnorm)
+
+SampleN.ThreeArm <- function(alpha, beta, piE, piR ,piP, Delta){
+
+  myfunc <- function(CP){
+    nE <- N/(1+CR+CP)
+    rho <- -piR*(1-piR)*sqrt(CP)
+    rho <- rho/sqrt(piE*(1-piE)*CR+piR*(1-piR))
+    rho <- rho/sqrt(piR*(1-piR)*CP+piP*(1-piP)*CR)
+    Sigma <- matrix(c(1, rho, rho, 1), nrow = 2, ncol = 2, byrow = T)
+              
+    a <- 1+CR+CP  
+    b <- -(1+CR+CP+piE+CR*piR+CP*piP+Delta*(1+2*CR+CP))  
+    c <- CR*Delta^2+(1+CR+CP+2*CR*piR)*Delta+piE+CR*piR+CP*piP
+    d <- -CR*piR*Delta*(Delta+1)
+    v <- (b^3)/(27*a^3)-(b*c)/(6*a^2)+d/(2*a)    
+    u <- sign(v)*sqrt((b^2/(9*a^2))-(c/(3*a)))
+    w <- (pi+acos(v/(u^3)))/3
+    piRs <- 2*u*cos(w)-b/(3*a)
+
+    c1 <- (piRs-Delta)*(1-piRs+Delta)*CR+piRs*(1-piRs)
+    c1 <- c1/(piE*(1-piE)*CR+piR*(1-piR))
+    c1 <- sqrt(c1)*qnorm(1-alpha/2)
+    c1 <- c1-(piE-(piR-Delta))*sqrt(CR*nE)/sqrt(piE*(1-piE)*CR+piR*(1-piR))
+
+    c2 <- piRs*(1-piRs)*CP+(piRs-Delta)*(1-piRs+Delta)*CR
+    c2 <- c2/(piR*(1-piR)*CP+piP*(1-piP)*CR)
+    c2 <- sqrt(c2)*qnorm(1-alpha/2)
+    c2 <- c2-(piR-(piP+Delta))*sqrt(CR*CP*nE)/sqrt(piR*(1-piR)*CP+piP*(1-piP)*CR)
+
+    fv <- pmvnorm(lower = c(c1, c2), upper = c(Inf, Inf), mean = c(0, 0), corr = Sigma)
+    return(fv)
+  }
+
+  delta1 <- piE-(piR-Delta)
+  delta2 <- piR-(piP+Delta)
+  CR <- 1
+  CP <- 1/sqrt(2)
+
+  N <- 1
+  while( myfunc(CP) < 1-beta ) N <- N+1
+ 
+  ans <- optim(par = 0.5, fn = myfunc, method = "Brent", lower = 0, upper = 1, control = list(fnscale = -1))
+  while( ans$value > 1-beta ){
+    N <- N-1
+    ans <- optim(par = 0.5, fn = myfunc, method = "Brent", lower = 0, upper = 1, control = list(fnscale = -1))
+  }
+  nE <- round((N+1)/(1+CR+ans$par), 0)
+  nR <- round(nE*CR, 0)
+  nP <- round(nE*ans$par, 0)
+  TN <- sum(nE, nR, nP)
+  return(c(TN, ans$par, ans$value, nE, nR, nP))
+}
+
+# Example
+# result <- paste(c('Total sample size', 'CP', 'power', 'nE', 'nR', 'nP'), SampleN.ThreeArm(alpha = 0.05, beta = 0.2, piE = 0.45, piR = 0.4, piP = 0.1, Delta = 0.1), sep = ':')
+
---
+++ Three Arm NI_continuous.R
@@ -0,0 +1,56 @@
+library(mvtnorm)
+
+ThreeArmsTrial <- function(alpha, beta, muE, muR, muP, sigma, M1, M2, k, code = 1){
+  func.H <- function(piE, piP){
+    nR <- N/(1+k*piE+piP)
+    rho <- sqrt(piE*piP/((1+piE)*(1+piP)))
+    corr.matrix <- array(c(1, -rho, -rho, 1), dim = c(2, 2))
+    c <- qmvnorm(1-alpha, tail = "lower", mean = c(0, 0), corr = corr.matrix)$quantile  
+    a <- -c+delta1*sqrt(piE*nR/(1+piE))
+    b <- -c+delta2*sqrt(piP*nR/(1+piP))
+    v <- pmvnorm(lower = c(-Inf, -Inf), upper = c(a, b), mean = c(0, 0), corr = corr.matrix)
+    return(v)
+  }
+     
+  # a larger value implies the better efficacy 
+  if(code == 1){
+    delta1 <- (muE-muR+M2)/sigma  # delta1, delta2 > 0
+    delta2 <- (muR-muP-M1)/sigma
+  }
+  ## a smaller value implies the better efficacy
+  if(code == 2){
+    delta1 <- (muR-muE+M2)/sigma  # delta1, delta2 > 0
+    delta2 <- (muP-muR-M1)/sigma
+  }
+
+  piE <- piP <- 1/sqrt(k+1)
+    
+  #################### step (a)_Initial total sample size ####################
+  N <- 1
+  while(func.H(piE, piP) < 1-beta) { N <- N+1 }
+  #################### step(b)_optimal sample size allocation ####################   
+  func <- function(x){
+    piE <- x[1]
+    piP <- x[2]
+    v <- func.H(piE, piP)
+    return(v)
+  }
+  ans <- optim(par = c(0.5, 0.5), fn = func, method = "L-BFGS-B",
+               lower = c(0, 0), upper = c(1, 1), control = list(fnscale = -1))
+  while(ans$value > 1-beta){
+    N <- N-1
+    ans <- optim(par = c(0.5, 0.5), fn = func, method = "L-BFGS-B",
+                 lower = c(0, 0), upper = c(1, 1), control = list(fnscale = -1))
+  }
+  nR <- round((N+1)/(1+k*ans$par[1]+ans$par[2]), 0)
+  nE <- round(nR*ans$par[1], 0)
+  nP <- round(nR*ans$par[2], 0)
+  TN <- nR+k*nE+nP
+  return(c(TN, ans$par, ans$value, nR, nE, nP))
+  #return(proc.time())
+}
+# Example
+#result <- print(paste(c('Total sample size', 'piE', 'piP', 'power', 'nR', 'nE', 'nP'), 
+#				ThreeArmsTrial(alpha = 0.05, beta = 0.2, muE = 0.85, muR = 0.8, muP = 0.5, sigma = 0.2, M1 = 0.15, M2 = 0.025, k = 2), 
+#				sep = ':'))
+
